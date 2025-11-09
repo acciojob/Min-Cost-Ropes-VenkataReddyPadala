1b87e920d6e833811a74ef6d7a7df4ebe5d9c3f0
VenkataReddyPadala <84061954+VenkataReddyPadala@users.noreply.github.com>
2025-11-09 12:01:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+		let res=0;
+  while(arr.length>1){
+	  let sum=0;
+	  arr.sort((a,b)=>a-b);
+	  sum=arr[0]+arr[1];
+	  arr.splice(0,2);
+	  res+=sum;
+	  arr.push(sum);
+  }
+	return res;
 }
 
 module.exports=mincost;
